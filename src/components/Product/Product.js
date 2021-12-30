7b6639eb049c53324d6e9065bc6166f612e6de56
Ezequielpereyraa <ezequielpereyra@hotmail.com>
2021-12-29 21:43:16 -0300

    change in card

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,8 +7,8 @@ const Product = ({ product, price, description, quantity, unity, img }) => {
   return (
     <div className="product">
       {img && <img className="product__img" src={img} alt={product} />}
+      {product && <h4 className="product__title">{product}</h4>}
       <div className="product__info">
-        {product && <h4 className="product__title">{product}</h4>}
         {description && <p className={'product__description'}>{description}</p>}
         {price
           ? (
